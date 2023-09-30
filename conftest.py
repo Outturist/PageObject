d0f0f0f84234b715426df6a4ba3d057d504f9734
kharkov <Andrey.Kharkov@mvideo.ru>
2023-09-30 10:04:58 +0300

    created structure without locators added test_main_page.py, __init__.py

--- conftest.py
+++ conftest.py
@@ -4,8 +4,8 @@ from selenium.webdriver.chrome.options import Options
 
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name')
-    parser.addoption('--user_language')
+    parser.addoption('--browser_name', default='chrome')
+    parser.addoption('--language')
 
 
 @pytest.fixture
@@ -13,7 +13,7 @@ def browser(request):
     browser_name = request.config.getoption('browser_name')
     browser = None
     if browser_name == 'chrome':
-        user_language = request.config.getoption('user_language')
+        user_language = request.config.getoption('language')
         options = Options()
         options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         browser = webdriver.Chrome(options=options)
